797a0f6754da996ecd8efc6879e9182ae7c96256
Victoria <victoriakarenlo@gmail.com>
2022-03-27 22:17:42 +0000

    formated restuarants and attractions to cards

--- static/js/get-attractions.js
+++ static/js/get-attractions.js
@@ -11,23 +11,33 @@ attractionBtn.addEventListener('click', () => {
   // creating another promise (then) to do something else with that data
   .then(data => { // take this data and iterate over it and take what's important and add to html
     // name, rating, photos
-    let attrList = []
-    const results = JSON.parse(data)
-    for (const result of results.results) {
-      attrList.push(`${result.name}`)
-    };
+
+    const attrObj = JSON.parse(data);
+    console.log(attrObj);
 
     // clears anything in the HTML and prevents user from seeing duplicate request data
     document.querySelector('#attractions-data').innerHTML = ""
 
     // iterate through the list of restaurants and display in an input form
     // for of will return the value, not the index
-    for (let attrItem of attrList) {
+    for (const result of attrObj.results) {
 
-      document.querySelector('#attractions-data').innerHTML += `<div>
-      <input type="checkbox" id="${attrItem}" name="attr-choice" value="${attrItem}">
-      <label for="${attrItem}">${attrItem}</label>
-      </div>`
+      if (result.business_status === "OPERATIONAL") {
+        // result.photos = a list of objects. need to parse the data to get photo
+
+
+        document.querySelector('#attractions-data').innerHTML += `
+        <div class="card mb-3">
+          <!-- <p>${result.photos}</p> -->
+          <!-- <img class="card-img-top" src="${result.photos}" alt="Card image cap"> -->
+            <div class="card-body">
+              <h5 class="card-title">${result.name}</h5>
+              <input type="checkbox" id="${result.name}" name="rest-choice" value="${result.name}">
+              <label for="${result.name}">${result.name} (${result.rating} rating)</label>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer. ${result.rating} rating </p>
+            </div>
+        </div>`
+      }
     };
   })
 });
--- static/js/get-restaurants.js
+++ static/js/get-restaurants.js
@@ -11,24 +11,35 @@ restButton.addEventListener('click', () => {
   // creating another promise (then) to do something else with that data
   .then(data => { // take this data and iterate over it and take what's important and add to html
     // name, rating, photos
-    let restList = []
-    const results = JSON.parse(data)
-    for (const result of results.results) {
-      restList.push(`${result.name}`)
-    };
+
+    const restObj = JSON.parse(data);
+    console.log(restObj);
 
     // clears anything in the HTML and prevents user from seeing duplicate request data
-    document.querySelector('#restaurants-data').innerHTML = ""
+    document.querySelector('#restaurants-data').innerHTML = "";
+    
+    for (const result of restObj.results) {
 
-    // iterate through the list of restaurants and display in an input form
-    // for of will return the value, not the index
-    for (let restItem of restList) {
+      if (result.business_status === "OPERATIONAL") {
+        // result.photos = a list of objects. need to parse the data to get photo
 
-      document.querySelector('#restaurants-data').innerHTML += `<div>
-      <input type="checkbox" id="${restItem}" name="rest-choice" value="${restItem}">
-      <label for="${restItem}">${restItem}</label>
-      </div>`
+        document.querySelector("#restaurants-data").innerHTML += `
+        <div class="card mb-3">
+          <!-- <p>${result.photos}</p> -->
+          <!-- <img class="card-img-top" src="${result.photos}" alt="Card image cap"> -->
+            <div class="card-body">
+              <h5 class="card-title">${result.name}</h5>
+              <input type="checkbox" id="${result.name}" name="rest-choice" value="${result.name}">
+              <label for="${result.name}">${result.name} (Rating: ${result.rating} stars)</label>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer. ${result.rating} rating </p>
+            </div>
+        </div>`
+      }
     };
-  })
+
+    }
+  )
 });
+
+
 
